1299ecb4e6cb1acad9fa53ce573a0e15bed521ee
Riccardo Gherardi <gherardr@amazon.com>
2017-12-02 20:48:32 -0800

    Amended solution to problem #1 Now with proper stack and queue constraints

--- daily_coding_problem.py
+++ daily_coding_problem.py
@@ -1,22 +1,40 @@
-from itertools import chain, izip, islice
+from collections import deque
 import numpy as np
 
 
 def coding_problem_1(l):
     """
     Given a stack of N elements, interleave the first half of the stack
-    with the second half reversed using one other queue. Example:
-    >>> list(coding_problem_1([1, 2, 3, 4, 5]))
+    with the second half reversed using one other queue.
+    Example:
+
+    >>> coding_problem_1([1, 2, 3, 4, 5])
     [1, 5, 2, 4, 3]
+    >>> coding_problem_1([1, 2, 3, 4, 5, 6])
+    [1, 6, 2, 5, 3, 4]
+
+    Note: with Python lists, you could instead islice(chain.from_iterable(izip(l, reversed(l))), len(l))
     """
-    return islice(chain.from_iterable(izip(l, reversed(l))), len(l))
+    queue = deque([])  # stack S:[1,2,3,4,5], queue Q:[]
+    for cnt in xrange(len(l) - 1):  # move stack into queue. S:[1], Q:[5,4,3,2]
+        queue.append(l.pop())
+    for cnt in xrange(len(queue) / 2):
+        l.append(queue.popleft())  # S:[1,5], Q:[4,3,2]
+        for cnt2 in xrange(len(queue) - 1):  # rotate last element to front, S:[1,5], Q:[2,4,3]
+            queue.append(queue.popleft())
+        l.append(queue.popleft())  # S:[1,5,2], Q:[4,3]
+    if queue:
+        l.append(queue.popleft())
+    return l
 
 
 def coding_problem_2(l):
     """
     Given an array of integers, return a new array such that each element at index i of
     the new array is the product of all the numbers in the original array except the one at i.
-    Solve it without using division and in O(n). Example:
+    Solve it without using division and in O(n).
+    Example:
+
     >>> coding_problem_2([1, 2, 3, 4, 5])
     [120, 60, 40, 30, 24]
     """
