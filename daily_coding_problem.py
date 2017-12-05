a7558ddbcdf49e784550da92663e0d6545a21150
Riccardo Gherardi <gherardr@amazon.com>
2017-12-05 12:54:58 -0800

    Adding solution to problem 5

--- daily_coding_problem.py
+++ daily_coding_problem.py
@@ -109,6 +109,27 @@ def coding_problem_4(arr):
     return 1 + (arr[-1] if len(idxs) == 0 else idxs[0])
 
 
+def coding_problem_5():
+    """
+    cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.
+    Given this implementation of cons below, implement car and cdr.
+    For examples, car(cons(3, 4)) == 3 and cdr(cons(3, 4)) == 4
+
+    >>> coding_problem_5()
+    True
+    """
+    def cons(a, b):
+        return lambda f: f(a, b)
+
+    def car(f):
+        return f(lambda a, b: a)
+
+    def cdr(f):
+        return f(lambda a, b: b)
+
+    return car(cons(3, 4)) == 3 and cdr(cons(3, 4)) == 4
+
+
 if __name__ == '__main__':
 
     import doctest
