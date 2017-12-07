a4c0d5ae96ee5dad47ba24a1c14b771c59c25159
Riccardo Gherardi <gherardr@amazon.com>
2017-12-06 16:55:25 -0800

    Adding solution to problem 6

--- daily_coding_problem.py
+++ daily_coding_problem.py
@@ -113,7 +113,7 @@ def coding_problem_5():
     """
     cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.
     Given this implementation of cons below, implement car and cdr.
-    For examples, car(cons(3, 4)) == 3 and cdr(cons(3, 4)) == 4
+    Examples: car(cons(3, 4)) == 3, cdr(cons(3, 4)) == 4
 
     >>> coding_problem_5()
     True
@@ -128,6 +128,69 @@ def coding_problem_5():
         return f(lambda a, b: b)
 
     return car(cons(3, 4)) == 3 and cdr(cons(3, 4)) == 4
+
+
+def coding_problem_6():
+    """
+    An XOR linked list is a more memory efficient doubly linked list.
+    Instead of each node holding next and prev fields, it holds a field named both, which is a XOR of the next node
+    and the previous node. Implement a XOR linked list; it has an add(element) which adds the element to the end,
+    and a get(index) which returns the node at index.
+    Example:
+
+    >>> coding_problem_6()
+    True
+
+    Note: python does not have actual pointers (id() exists but it is not an actual pointer in all implementations).
+    For this reason, we use a python list to simulate memory. Indexes are the addresses in memory.
+    """
+    class XORLinkedListNode(object):
+
+        def __init__(self, val, prev, next):
+            self.val = val
+            self.both = prev ^ next
+
+        def next_node(self, prev_idx):
+            return self.both ^ prev_idx
+
+        def prev_node(self, next_idx):
+            return self.both ^ next_idx
+
+    class XORLinkedList(object):
+
+        def __init__(self):
+            self.memory = [XORLinkedListNode(None, -1, -1)]
+
+        def add(self, val):
+            current_node_index = 0
+            previous_node_index = -1
+            current_node = self.memory[current_node_index]
+
+            while True:  # walk down the list
+                next_node_index = current_node.next_node(previous_node_index)
+                if next_node_index == -1:  # we reached the end on the list
+                    break
+                previous_node_index, current_node_index = current_node_index, next_node_index
+                current_node = self.memory[next_node_index]
+
+            new_node_index = len(self.memory)  # "allocation"
+            current_node.both = previous_node_index ^ new_node_index
+            self.memory.append(XORLinkedListNode(val, current_node_index, -1))
+
+        def get(self, index):
+            current_index = 0
+            previous_index = -1
+            current_node = self.memory[0]
+            for cnt in xrange(index + 1):
+                previous_index, current_index = current_index, current_node.next_node(previous_index)
+                current_node = self.memory[current_index]
+            return current_node.val
+
+    l = XORLinkedList()
+    for cnt in xrange(0, 4):
+        l.add(cnt)
+
+    return l.get(2) == 2
 
 
 if __name__ == '__main__':
