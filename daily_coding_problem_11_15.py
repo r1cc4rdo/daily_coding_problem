a4ea336a7c9862d38e3614afe96ef9b6fb1c65fc
Riccardo Gherardi <gherardr@amazon.com>
2017-12-15 00:44:08 -0800

    Added problem #14

--- daily_coding_problem_11_15.py
+++ daily_coding_problem_11_15.py
@@ -17,7 +17,7 @@ def coding_problem_11(strings, prefix):
         """
         Pretty pleased with this binary search function. Supports searching for the first, last or any match of
         the given item. Both the elements of the list and the item can be optionally mapped through a function which
-        should output items that support the standard ordering operators.
+        should output objects supporting the standard ordering operators <=>.
         """
         fmap = fmap if fmap else lambda x: x
         fitem = fmap(item)
@@ -130,6 +130,39 @@ def coding_problem_13(s, k):
         max_length = max(max_length, end_index - start_index)
 
     return max_length
+
+
+def coding_problem_14():
+    """
+    The area of a circle is defined as $\pi r^2$. Estimate $\pi$ to 3 decimal places using a Monte Carlo method.
+    Example:
+
+    >>> import math
+    >>> import random
+    >>> random.seed(0xBEEF)
+    >>> pi_approx = coding_problem_14()
+    >>> abs(math.pi - pi_approx) < 1e-2
+    True
+
+    Note: the unit test above is not testing for 3 decimal places, but only 2. Getting to 3 significant digits would
+    require too much time each time, since convergence is exponentially slow. Also priming the random number generator
+    to avoid random failure for unlucky distributions of samples.
+    """
+    import math
+    import random
+
+    inside = samples = cnt = 0
+    pi_approx = 3.0  # physicist $/pi$
+    while True:
+
+        for cnt in xrange(10000):
+
+            inside += math.hypot(random.random(), random.random()) <= 1
+
+        samples += cnt + 1
+        prev_pi, pi_approx = pi_approx, 4 * float(inside) / samples
+        if abs(pi_approx - prev_pi) < 1e-5:
+            return pi_approx
 
 
 if __name__ == '__main__':
