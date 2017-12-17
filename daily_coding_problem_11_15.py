b8922a61c2b9538e79b692ff129dc155f9e12ccf
Riccardo Gherardi <gherardr@amazon.com>
2017-12-16 20:10:34 -0800

    Added problem #15, #16, updated #11

--- daily_coding_problem_11_15.py
+++ daily_coding_problem_11_15.py
@@ -18,6 +18,11 @@ def coding_problem_11(strings, prefix):
         Pretty pleased with this binary search function. Supports searching for the first, last or any match of
         the given item. Both the elements of the list and the item can be optionally mapped through a function which
         should output objects supporting the standard ordering operators <=>.
+
+        Note: leaving this here for historical reasons, but I have learned that the functionalities provided below
+        are offered by the module bisect and the functions bisect_left and bisect_right (well, with the exception of
+        fmap argument -- to achieve the same effect with bisect you would have to wrap a list into a custom map
+        class exposing the __getitem__ method, and possibly all the other parts of the indexing interface).
         """
         fmap = fmap if fmap else lambda x: x
         fitem = fmap(item)
@@ -61,11 +66,11 @@ def coding_problem_11(strings, prefix):
 
                 first = midpoint + 1
 
-        return first if fmap(list_like[first]) == fitem else -1
+        return fmap(list_like[first]) == fitem, first
 
     strings = [s.lower() for s in sorted(strings)]
-    s = binary_search(strings, prefix, 'first', fmap=lambda s: s[:min(3, len(s))])
-    e = binary_search(strings, prefix, 'last', fmap=lambda s: s[:min(3, len(s))])
+    _, s = binary_search(strings, prefix, 'first', fmap=lambda s: s[:min(3, len(s))])
+    _, e = binary_search(strings, prefix, 'last', fmap=lambda s: s[:min(3, len(s))])
     return strings[s:(e+1)]
 
 
@@ -163,6 +168,66 @@ def coding_problem_14():
         prev_pi, pi_approx = pi_approx, 4 * float(inside) / samples
         if abs(pi_approx - prev_pi) < 1e-5:
             return pi_approx
+
+
+def coding_problem_15(sample_generator):
+    """
+    Given a stream of elements too large to store in memory, pick a random element from the stream with
+    uniform probability.
+    Example:
+
+    >>> import random
+    >>> random.seed(0xBADC0FFE)
+    >>>
+    >>> def sample_gen_fun(n):
+    ...     for x in xrange(n):
+    ...         yield x
+    >>>
+    >>> num = 10
+    >>> hist = [0] * num
+    >>> for trials in xrange(5000):
+    ...
+    ...     hist[coding_problem_15(sample_gen_fun(num))] += 1
+    >>>
+    >>> all(abs(float(h) / sum(hist) - 1.0 / len(hist)) < 0.01 for h in hist)
+    True
+
+    Note: this problem only makes sense if you don't know the total number in advance. Otherwise, you would pick a
+    random integer between [0..len[ and record the element corresponding to that index when processed. Setting a seed
+    for the random number generator to avoid random failures due to unlucky distributions of samples.
+    """
+    import random
+
+    """    
+    sample_count = 0
+    selected_sample = None
+    rand_index = random.randrange(2**32)
+    for sample in sample_generator:
+
+        sample_count += 1
+        if rand_index % sample_count == 0:
+            selected_sample = sample
+
+    return selected_sample
+
+    The code above was my first take.
+    Does not work, because the modulus of a random integer is uniformly distributed between ALL the values that came
+    before and we are not allowed to store them (too big to fit in memory requirement). The solution is instead what
+    implemented below: assuming that at round n we have already selected between the available samples with uniform
+    probability, accept the incoming one as the new selected sample with a probability that keeps everyone's chances
+    fair. Example: at n==1, the first sample as 100% chance of being selected. n==2, second sample 50% chance, n==3
+    1/3 chance etc. Proof: for n==5, the prob. of choosing first sample is 1 * 0.5 * 0.666.. * 0.75 * 0.8 = 0.2 = 1/5
+    """
+
+    sample_count = 0
+    selected_sample = None
+    for sample in sample_generator:
+
+        sample_count += 1
+        if random.random() <= 1.0 / sample_count:
+            selected_sample = sample
+
+    return selected_sample
 
 
 if __name__ == '__main__':
