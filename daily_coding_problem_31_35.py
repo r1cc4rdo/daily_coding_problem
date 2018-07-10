a6d5c3fdff3ae29b897291cf3229571c7a95fea7
Riccardo Gherardi <gherardr@amazon.com>
2018-07-10 14:58:46 -0700

    Add #35

--- daily_coding_problem_31_35.py
+++ daily_coding_problem_31_35.py
@@ -1,8 +1,8 @@
 def coding_problem_31():
     """
     The edit distance between two strings refers to the minimum number of character insertions, deletions, and
-    substitutions required to change one string to the other. For example, the edit distance between “kitten” and
-    “sitting” is three: substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
+    substitutions required to change one string to the other. For example, the edit distance between "kitten" and
+    "sitting" is three: substitute the "k" for "s", substitute the "e" for "i", and append a "g".
     Given two strings, compute the edit distance between them.
 
     >>> coding_problem_31()
@@ -57,19 +57,58 @@ def coding_problem_34():
     pass
 
 
-def coding_problem_35():
+def coding_problem_35(rgbs):
     """
     Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the 'R's
     come first, the 'G's come second, and the 'B's come last. You can only swap elements of the array.
-    Do this in linear time and in-place.
+    Do this in linear time and in-place. For example:
 
-    For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become:
-    ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+    >>> coding_problem_35(['G', 'B', 'R', 'R', 'B', 'R', 'G'])
+    ['R', 'R', 'R', 'G', 'G', 'B', 'B']
 
-    >>> coding_problem_35()
+    The problem can be solved by (insertion) sorting as follows.
+    This solution does not take advantage of the expected high number of equal value elements.
 
+    >>> rgbs = ['G', 'B', 'R', 'R', 'B', 'R', 'G']
+    >>> for i in range(len(rgbs) - 1):
+    ...     for j in range(i + 1, len(rgbs)):
+    ...         if rgbs[i] < rgbs[j]:  # 'R' > 'G' > 'B'
+    ...             rgbs[i], rgbs[j] = rgbs[j], rgbs[i]
+    >>> rgbs
+    ['R', 'R', 'R', 'G', 'G', 'B', 'B']
+
+    Sorting as described above has a complexity of O(n^2).
+    The following solution does at most two passes, and therefore is O(n).
     """
-    pass
+    left_index, right_index = 0, len(rgbs) - 1
+    while True:  # move Rs to front
+
+        while rgbs[left_index] == 'R' and left_index < right_index:  # advance to first non R
+            left_index += 1
+
+        while rgbs[right_index] != 'R' and left_index < right_index:  # regress to last R
+            right_index -= 1
+
+        if left_index >= right_index:
+            break
+
+        rgbs[left_index], rgbs[right_index] = rgbs[right_index], rgbs[left_index]
+
+    right_index = len(rgbs) - 1
+    while True:  # move Bs to tail
+
+        while rgbs[left_index] != 'B' and left_index < right_index:  # advance to first B
+            left_index += 1
+
+        while rgbs[right_index] == 'B' and left_index < right_index:  # regress to last non B
+            right_index -= 1
+
+        if left_index >= right_index:
+            break
+
+        rgbs[left_index], rgbs[right_index] = rgbs[right_index], rgbs[left_index]
+
+    return rgbs
 
 
 if __name__ == '__main__':
