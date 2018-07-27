6749715648ff7afa1f8d50a11bd9affc73a256bb
Riccardo Gherardi <gherardr@amazon.com>
2018-07-26 18:43:50 -0700

    Add #40

--- daily_coding_problem_36_40.py
+++ daily_coding_problem_36_40.py
@@ -1,3 +1,6 @@
+import math
+
+
 def coding_problem_36(tree):
     """
     Given the root to a binary search tree, find the second largest node in the tree.
@@ -82,16 +85,27 @@ def coding_problem_39():
     pass
 
 
-def coding_problem_40():
+def coding_problem_40(numbers):
     """
     Given an array of integers where every integer occurs three times except for one integer, which only occurs once,
     find and return the non-duplicated integer. Do this in O(N) time and O(1) space.
-    For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], return 19.
+    Examples:
 
-    >>> coding_problem_40()
+    >>> coding_problem_40([6, 1, 3, 3, 3, 6, 6])
+    1
+    >>> coding_problem_40([13, 19, 13, 13])
+    19
 
+    Note: the code below fails for negative numbers, but you could shift the entire array by the minimum.
     """
-    pass
+    bits = [0] * int(math.ceil(math.log(max(numbers), 2)))  # low endian
+    for number in numbers:
+        index = 0
+        while number > 0:
+            bits[index] += number & 1
+            number //= 2
+            index += 1
+    return sum((b % 3) * 2**i for i, b in enumerate(bits))
 
 
 if __name__ == '__main__':
