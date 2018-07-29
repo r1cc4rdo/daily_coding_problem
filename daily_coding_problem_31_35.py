55de01d2312b5bf2b0332bfaa7ce710d329588f0
Riccardo Gherardi <gherardr@amazon.com>
2018-07-29 12:20:03 -0700

    Add #31

--- daily_coding_problem_31_35.py
+++ daily_coding_problem_31_35.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def coding_problem_31(s, t):
+def coding_problem_31(s, t, debt=0):
     """
     Given two strings, compute the edit distance between them.
     The edit distance between two strings refers to the minimum number of character insertions, deletions, and
@@ -10,8 +10,20 @@ def coding_problem_31(s, t):
 
     >>> coding_problem_31("kitten", "sitting")  # k>>s, e>>i, +g
     3
+    >>> coding_problem_31("kitten", "cat")  # k>>c, i>>a, -ten
+    5
+    >>> coding_problem_31("black", "white")
+    5
+    >>> coding_problem_31("top", "dog")
+    2
     """
-    pass
+    if not s or not t:
+        return len(s) + len(t) + debt
+
+    i = coding_problem_31(s, t[1:], debt + 1)  # insertion
+    d = coding_problem_31(s[1:], t, debt + 1)  # deletion
+    s = coding_problem_31(s[1:], t[1:], debt + (s[0] != t[0]))  # substitution / matching character
+    return min(i, d, s)
 
 
 def coding_problem_32(exchange_matrix):
