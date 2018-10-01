b23eebf9508a9161d6c5413cfa8197b85f8cd36d
P Mairo <akoudanilo@gmail.com>
2018-10-01 18:30:22 +0200

    Add daily_coding_problem_50.py
    programme de calcul .. me voici revenu en 5ème

---
+++ daily_coding_problem_50.py
@@ -0,0 +1,17 @@
+"""
+Suppose an arithmetic expression is given as a binary tree. Each leaf is an integer and each internal 
+node is one of '+', '−', '∗', or '/'.
+
+Given the root to such a tree, write a function to evaluate it.
+
+For example, given the following tree:
+
+    *
+   / \
+  +    +
+ / \  / \
+3  2  4  5
+You should return 45, as it is (3 + 2) * (4 + 5).
+"""
+
+# Compilers ? I think so, evaluating from top to bottom
