f93859908b589178446364587003abf5ec595aea
P Mairo <akoudanilo@gmail.com>
2018-10-09 20:52:26 +0200

    Update daily_coding_problem_50.py
    Coding actually

--- daily_coding_problem_50.py
+++ daily_coding_problem_50.py
@@ -14,4 +14,29 @@ For example, given the following tree:
 You should return 45, as it is (3 + 2) * (4 + 5).
 """
 
-# Compilers ? I think so, evaluating from top to bottom
+from nose.tools import assert_equal
+
+tab = ['*', '+', '+', 3, 2, 4, 5]     # Storing the array as a table 
+
+def eval(tab, i):
+    op = tab[i]
+    lft = left(i)
+    rgt = right(i)
+    if op == "+":
+        return eval(tab, lft) + eval(tab, rgt)
+    elif op == "-":
+        return eval(tab, lft) - eval(tab, rgt)
+    elif op == "*":
+        return eval(tab, lft) * eval(tab, rgt)
+    elif op == "/":
+        return eval(tab, lft) / eval(tab, rgt)
+    else: 
+        return op
+    
+def left(i):
+    return int(2*i + 1)
+def right(i):
+    return int(2*i + 2)
+
+print(assert_equal(eval(['*', '+', 4, 5, 6], 0), 44))
+print(assert_equal(eval(['*', 4, '+', ' ', ' ', 5, 6], 0), 44))
