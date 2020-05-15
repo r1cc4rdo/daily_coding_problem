c85a0f9616b7f07fcb709a408891b2040c82b91e
vaibhavpandey11 <61282732+vaibhavpandey11@users.noreply.github.com>
2020-05-15 21:20:47 +0530

    Solution from https://github.com/vaibhavpandey11/daily_coding_problem
    There are solutions to some other problems of dailycodingproblem.com at the following repository -
    https://github.com/vaibhavpandey11/daily_coding_problem

--- problems/46/solution_46.py
+++ problems/46/solution_46.py
@@ -1,21 +1,23 @@
-def coding_problem_46(str):
-    """
-    Given a string, find the longest palindromic contiguous substring. If there are more than one with the maximum
-    length, return any one. Examples:
+'''
+This problem was asked by Amazon.
+Given a string, find the longest palindromic contiguous substring.
+If there are more than one with the maximum length, return any one.
+For example, the longest palindromic substring of "aabcdcb" is "bcdcb".
+The longest palindromic substring of "bananas" is "anana".
+'''
 
-    >>> coding_problem_46("aabcdcb")
-    'bcdcb'
-    >>> coding_problem_46("bananas")
-    'anana'
-    """
-    for length in range(len(str), 0, -1):
-        for offset in range(len(str) - length + 1):
-            sub_str = str[offset:offset + length]
-            if sub_str == sub_str[::-1]:
-                return sub_str
+#________________________________________________________________
 
+def is_palindrome(x):  # checks if a string is palindrome
+    for i in range(len(x)//2):
+        if x[i] != x[-1 - i]: return False
+    else: return True
 
-if __name__ == '__main__':
+string, long_palindromes = input(), (("", 0),)  # 'long_palindromes' is the tuple of longest palindrome
+substrings = [string[i: i+j] for i in range(len(string)) for j in range(1, len(string) - i + 1)]  # finding all substrings in the input string
 
-    import doctest
-    doctest.testmod(verbose=True)
+for substring in substrings:
+    if is_palindrome(substring) and len(substring) > long_palindromes[-1][-1]: long_palindromes = ((substring, len(substring)),)  # when substring (which is a palindrome) of length greater than that of the previous one is found
+    elif is_palindrome(substring) and len(substring) == long_palindromes[-1][-1]: long_palindromes = long_palindromes + ((substring, len(substring)),)  # when substring (which is a palindrome) of length equal to that of the previous one is found
+
+for long_palindrome in set(long_palindromes): print(long_palindrome[0])  # typecast to set to remove duplicate palindromes, if exist
